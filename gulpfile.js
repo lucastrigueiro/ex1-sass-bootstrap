2a5fc1e9bc0d776d79592c5e9fbc469302990a4a
Lucas Henrique Trigueiro <lucashtrigueiro@gmail.com>
2017-11-29 04:53:08 -0200

    Instalação de dependências para o uso do Sass

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+//Realiza a conversão de sass para css
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.sass')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+//Realiza a conversão de sass para css comprimido
+gulp.task('sass-comp', function () {
+ return gulp.src('./sass/**/*.sass')
+   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+   .pipe(gulp.dest('./css/min'));
+});
+
+//Observa a atualização de arquivos sass e então realiza as conversões
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.sass', ['sass', 'sass-comp']);
+});
+
+//Realiza as conversões incialmente e observa atualizações
+gulp.task('default', ['sass', 'sass-comp', 'sass:watch']);
